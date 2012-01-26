a982e508db0230ad886894e041f955e4dd72fb04
Lukas Vonlanthen <lukas.vonlanthen@gmail.com>
2012-01-26 16:57:19 +0100

    basic database setup

--- lmkp/__init__.py
+++ lmkp/__init__.py
@@ -1,7 +1,10 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
-from .models import DBSession
+import papyrus
+from papyrus.renderers import GeoJSON
+
+from .models.meta import DBSession
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -9,8 +12,14 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     config = Configurator(settings=settings)
+    config.include(papyrus.includeme)
+    config.add_renderer('geojson', GeoJSON())
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('db_test', '/db_test')
+    config.add_route('sample_values', '/sample_values/insert')
+    config.add_route('delete_sample_values', '/sample_values/delete')
+    config.add_route('geo_test', '/geo_test')
     config.scan()
     return config.make_wsgi_app()
 
