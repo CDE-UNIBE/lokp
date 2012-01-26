5dbe06ae4bc085d5503d0abe0c234abb247103b6
Lukas Vonlanthen <lukas.vonlanthen@gmail.com>
2012-01-26 16:57:19 +0100

    basic database setup

--- lmkp/scripts/populate.py
+++ lmkp/scripts/populate.py
@@ -9,11 +9,12 @@ from pyramid.paster import (
     setup_logging,
     )
 
-from ..models import (
+from ..models.meta import (
     DBSession,
-    MyModel,
     Base,
     )
+
+from ..models.database_objects import *
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
@@ -30,6 +31,8 @@ def main(argv=sys.argv):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
-    with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+    #===========================================================================
+    # with transaction.manager:
+    #    model = MyModel(name='one', value=1)
+    #    DBSession.add(model)
+    #===========================================================================
