95e009b35bdcd09be64c31edc312b29116c0a8db
Adrian Weber <adrian.weber@cde.unibe.ch>
2012-01-30 17:18:55 +0100

    Check user credentials against database

--- lmkp/views/login.py
+++ lmkp/views/login.py
@@ -1,21 +1,22 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
+__author__ = "Adrian Weber, Centre for Development and Environment, University of Bern"
+__date__ = "$Jan 20, 2012 10:39:24 AM$"
 
-from lmkp.security import USERS
+import logging
+from lmkp.security import check_user
 from pyramid.httpexceptions import HTTPFound
 from pyramid.security import forget
 from pyramid.security import remember
 from pyramid.view import view_config
 
-__author__ = "Adrian Weber, Centre for Development and Environment, University of Bern"
-__date__ = "$Jan 20, 2012 10:39:24 AM$"
+log = logging.getLogger(__name__)
 
 @view_config(route_name='login')
 def login(request):
-
-    print "login route request ************************************************"
-    print request
-
+    """
+    Login controller
+    """
     login_url = request.route_url('login')
     referrer = request.url
     if referrer == login_url:
@@ -30,14 +31,13 @@ def login(request):
     if 'form.submitted' in request.params:
         login = request.params['login']
         password = request.params['password']
-        if USERS.get(login) == password:
-            print 'login succeed **********************************************'
+        if check_user(login, password):
+            log.debug('Login succeed')
             headers = remember(request, login)
         else:
-            print 'login failed ***********************************************'
+            log.debug('Login failed')
             headers = forget(request)
 
-    print request
     return HTTPFound(location=came_from, headers=headers)
 
 @view_config(route_name='logout', renderer='lmkp:templates/index.pt')
--- lmkp/views/views.py
+++ lmkp/views/views.py
@@ -1,11 +1,7 @@
+from lmkp.models.database_objects import *
+from lmkp.models.meta import DBSession
 from pyramid.security import authenticated_userid
 from pyramid.view import view_config
-
-from ..models.meta import (
-    DBSession,
-    )
-
-from ..models.database_objects import *
 
 #@view_config(route_name='home', renderer='../templates/mytemplate.pt')
 def my_view(request):
@@ -14,7 +10,7 @@ def my_view(request):
 #    return {'one':one, 'project':'MyProject'}
     return {'one':'one', 'project':'MyProject'}
 
-@view_config(route_name='db_test', renderer='../templates/db_test.pt')
+@view_config(route_name='db_test', renderer='lmkp:templates/db_test.pt')
 def db_test(request):
     object = DBSession.query(A_Event).first()
     return {'object':object}
@@ -22,20 +18,24 @@ def db_test(request):
 @view_config(route_name='geo_test', renderer='geojson')
 def geo_test(request):
     return {
-            'type': 'Feature',
-            'id': 1,
-            'geometry': {'type': 'Point', 'coordinates': [53, -4]},
-            'properties': {'title': 'Dict 1'}
-            }
+        'type': 'Feature',
+        'id': 1,
+        'geometry': {'type': 'Point', 'coordinates': [53, -4]},
+        'properties': {'title': 'Dict 1'}
+        }
 
 @view_config(route_name='index', renderer='lmkp:templates/index.pt')
 def index(request):
-    print "index route request ************************************************"
-    print request
-    print request.url
-    lang='en'
+    """
+    Returns the main HTML page
+    """
+    lang = 'en'
+    # Check if the user is logged in
     username = authenticated_userid(request)
+    # Assume the user is not logged in per default
     login = False
     if username is not None:
         login = True
-    return {'header': 'welcome', 'login': login}
+    else:
+        username = 'unknown user'
+    return {'header': 'welcome', 'login': login, 'username': username}
