a0a134cda8207760d271a94f3856db7166f73e1f
Adrian Weber <adrian.weber@cde.unibe.ch>
2012-01-31 16:47:03 +0100

    Add toolbar to the map panel

--- lmkp/static/app/view/MapPanel.js
+++ lmkp/static/app/view/MapPanel.js
@@ -2,6 +2,20 @@ Ext.define('LMKP.view.MapPanel',{
     extend: 'GeoExt.panel.Map',
     alias: ['widget.mappanel'],
 
+    config: {
+        map: {}
+    },
+
+    map: {
+        layers: [new OpenLayers.Layer.OSM('Mapnik')]
+    },
+
+    tbar: [{
+        text: 'Zoom'
+    },{
+        text: 'Pan'
+    }],
+
     initComponent: function(){
         console.log('MapPanel init');
 
