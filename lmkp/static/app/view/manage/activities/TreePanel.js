c0bf25ec0b1d0b057183379b47c42454505dbfcd
Adrian Weber <adrian.weber@cde.unibe.ch>
2012-02-02 17:07:14 +0100

    Create a first draft layout for moderator view

---
+++ lmkp/static/app/view/manage/activities/TreePanel.js
@@ -0,0 +1,11 @@
+Ext.define('Lmkp.view.manage.activities.TreePanel',{
+    extend: 'Ext.panel.Panel',
+
+    alias: [ 'widget.manageactivitiestreepanel' ],
+
+    html: 'Zis is ze zreepanel',
+
+    initComponent: function(){
+        this.callParent(arguments);
+    }
+});
