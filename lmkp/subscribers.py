5630a0c71122eae8b402a34bba3f5eada9c9c6e4
Adrian Weber <adrian.weber@cde.unibe.ch>
2012-04-02 12:07:55 +0200

    Add user object to request

--- lmkp/subscribers.py
+++ lmkp/subscribers.py
@@ -1,8 +1,14 @@
+from lmkp.models.database_objects import User
+from lmkp.models.meta import DBSession as Session
+from logging import getLogger
 from pyramid.events import BeforeRender
 from pyramid.events import NewRequest
 from pyramid.events import subscriber
 from pyramid.i18n import TranslationStringFactory
 from pyramid.i18n import get_localizer
+from pyramid.security import authenticated_userid
+
+log = getLogger(__name__)
 
 tsf = TranslationStringFactory('lmkp')
 
@@ -29,3 +35,15 @@ def add_localizer(event):
         return localizer.translate(tsf(string))
     request.localizer = localizer
     request.translate = auto_translate
+
+def _get_user(request):
+    userid = authenticated_userid(request)
+    log.debug(userid)
+    if userid is not None:
+        user = Session.query(User).filter(User.username == userid).first()
+        return user
+    
+@subscriber(NewRequest)
+def add_user(event):
+    request = event.request
+    request.set_property(_get_user, 'user', reify=True)
