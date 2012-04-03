2cb37adf3a67a9859184f4324a75f24622f3a2e5
lukas.vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2012-04-03 11:39:33 +0200

    Changed layout of admin GUI, now uses panels.

--- lmkp/static/app/view/admin/MainPanel.js
+++ lmkp/static/app/view/admin/MainPanel.js
@@ -1,27 +1,33 @@
 Ext.define('Lmkp.view.admin.MainPanel', {
-	extend: 'Ext.panel.Panel',
+	extend: 'Ext.tab.Panel',
 	
 	alias: ['widget.adminmainpanel'],
 	
-	tbar: [{
-		text: 'Home',
-		xtype: 'button',
-		id: 'home'
-	}, {
-		text: 'YAML',
-		xtype: 'button',
-		menu: {
-			xtype: 'menu',
-			items: [{
-				text: 'Scan',
-				id: 'yaml_scan'
-			}]
-		}
-	}],
+	// tbar: [{
+		// text: 'Home',
+		// xtype: 'button',
+		// id: 'home'
+	// }, {
+		// text: 'YAML',
+		// xtype: 'button',
+		// menu: {
+			// xtype: 'menu',
+			// items: [{
+				// text: 'Scan',
+				// id: 'yaml_scan'
+			// }]
+		// }
+	// }],
+	
+	activeTab: 0,
 	
 	items: [{
+		title: 'Home',
 		xtype: 'adminhome'
 		// xtype: 'adminyamlscan'
+	}, {
+		title: 'YAML',
+		xtype: 'adminyamlscan'
 	}],
 	
 	initComponent: function() {
