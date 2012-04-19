f9a8578c2259f12437dd65e520811a8516268765
Adrian Weber <adrian.weber@cde.unibe.ch>
2012-04-19 14:23:21 +0200

    Clean up and add some comments

--- lmkp/views/changeset_protocol.py
+++ lmkp/views/changeset_protocol.py
@@ -1,26 +1,33 @@
 from lmkp.models.database_objects import *
 import logging
-from sqlalchemy.sql.expression import alias
-from uuid import UUID
 
 log = logging.getLogger(__name__)
 
 class ChangesetProtocol(object):
+    """
+    
+    """
 
     def __init__(self, Session, ** kwargs):
-
         self.Session = Session
 
     def read(self, request):
+        """
+        
+        """
 
+        # Join the activity changesets with the activities and the users
         a_changesetsJoins = self.Session.query(A_Changeset.source, Activity.activity_identifier.label("activity"), User.username).join(A_Changeset.user).join(A_Changeset.activity)
 
+        # Create a list of activity changesets
         activities = []
         for i in a_changesetsJoins.filter(self._filter_user(request)).limit(self._limit_changesets(request)):
             activities.append({'source': i[0], 'activity': str(i[1]), 'user': i[2]})
 
+        # Join the stakeholder changesets with the stakeholders and the users
         sh_changesetsJoins = self.Session.query(SH_Changeset.source, Stakeholder.stakeholder_identifier.label("stakeholder"), User.username).join(SH_Changeset.user).join(SH_Changeset.stakeholder)
 
+        # Create a list of stakeholder changesets
         stakeholders = []
         for i in sh_changesetsJoins.filter(self._filter_user(request)).limit(self._limit_changesets(request)):
             stakeholders.append({'source': i[0], 'stakeholder': str(i[1]), 'user': i[2]})
@@ -28,7 +35,9 @@ class ChangesetProtocol(object):
         return {'activities': activities, 'stakeholders': stakeholders}
 
     def _filter_user(self, request):
-
+        """
+        Returns a filter with the requested user or None
+        """
         user = request.params.get('user', None)
         if user is not None:
             return User.username == user
@@ -36,6 +45,9 @@ class ChangesetProtocol(object):
         return None
 
     def _limit_changesets(self, request):
+        """
+        Returns a limit or None
+        """
 
         limit = request.params.get('limit', None)
         if limit is not None:
--- lmkp/views/changesets.py
+++ lmkp/views/changesets.py
@@ -1,17 +1,12 @@
 from lmkp.models.meta import DBSession as Session
-from lmkp.renderers.renderers import translate_key
 from lmkp.views.changeset_protocol import ChangesetProtocol
 import logging
-from pyramid.i18n import TranslationStringFactory
 from pyramid.view import view_config
 
 log = logging.getLogger(__name__)
 
-_ = TranslationStringFactory('lmkp')
-
 changeset_protocol = ChangesetProtocol(Session)
 
-#@view_config(route_name='changesets_read', renderer='lmkp:templates/changeset_table.mak')
 @view_config(route_name='changesets_read', renderer='json')
 def read(request):
     return changeset_protocol.read(request)
