b61b1978715e2e05af8b485ea5907a29b61cb5b1
lukas.vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2012-04-25 10:01:19 +0200

    Adding further TagGroups still needs to be implemented.

--- lmkp/static/app/view/activities/NewActivityWindow.js
+++ lmkp/static/app/view/activities/NewActivityWindow.js
@@ -33,8 +33,8 @@ Ext.define('Lmkp.view.activities.NewActivityWindow', {
 					disabled: true,
 					text: 'Submit',
 					handler: function() {
-						var form = this.up('form').getForm();
-						if (form.isValid()) {
+						var theform = this.up('form').getForm();
+						if (theform.isValid()) {
 							// The form cannot be submitted 'normally' because ActivityProtocol expects a JSON object.
 							// As a solution, the form values are used to create a JSON object which is sent using an
 							// AJAX request.
@@ -54,6 +54,7 @@ Ext.define('Lmkp.view.activities.NewActivityWindow', {
 								jsonData: jsonData,
 								success: function() {
 									Ext.Msg.alert('Success', 'The activity was successfully created. It will be reviewed shortly.');
+									form.up('window').close();
 								},
 								failure: function() {
 									Ext.Msg.alert('Failure', 'The activity could not be created.');
@@ -71,6 +72,19 @@ Ext.define('Lmkp.view.activities.NewActivityWindow', {
 			});
 			// add the fields to the form
 			form.add(formConfig);
+			
+			// temp
+			form.add({
+				xtype: 'checkboxfield',
+				boxLabel: 'Add additional information',
+				submitValue: false,
+				handler: function(checkbox, checked) {
+					if (checked) {
+						Ext.Msg.alert('Coming soon ...', 'Here, the functionality to directly specify additional TagGroups in a next step will have to be implemented. See also discussion on Trac.');
+					}
+				}
+			});
+			
 			// add the form to the window
 			this.items = form;
 		}
