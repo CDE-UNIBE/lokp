c1e583b18e72ea0f6ea8a7916fda7a5f3e808ef8
lukas.vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2012-05-01 07:47:26 +0200

    User activities now with paging toolbar

--- lmkp/static/app/store/ActivityChangesets.js
+++ lmkp/static/app/store/ActivityChangesets.js
@@ -2,6 +2,19 @@ Ext.define('Lmkp.store.ActivityChangesets', {
 	extend: 'Ext.data.Store',
 	
 	requires: 'Lmkp.model.ActivityChangeset',
-	model: 'Lmkp.model.ActivityChangeset'
+	model: 'Lmkp.model.ActivityChangeset',
 	
+	pageSize: 10,
+	// remoteSort: true,
+	
+	proxy: {
+		type: 'ajax',
+		url: '/changesets',
+		reader: {
+			type: 'json',
+			root: 'activities',
+			totalProperty: 'activities_count'
+		},
+		startParam: 'offset'
+	}
 });
