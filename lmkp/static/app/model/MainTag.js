a89d949a65510090e30d0bea8ecb9a864a1cc03f
lukas.vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2012-05-15 14:48:03 +0200

    Updating TagGroups now matches redesign of main_tag.

---
+++ lmkp/static/app/model/MainTag.js
@@ -0,0 +1,14 @@
+Ext.define('Lmkp.model.MainTag', {
+	extend: 'Ext.data.Model',
+	
+	fields: [{
+		name: 'id',
+		type: 'string'
+	}, {
+		name: 'key',
+		type: 'string'
+	}, {
+		name: 'value',
+		type: 'string'
+	}],
+});
