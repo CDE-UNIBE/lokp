227527bb21fbb094f45f60b69a82335a8f4a071d
Adrian Weber <adrian.weber@cde.unibe.ch>
2012-05-16 13:37:53 +0200

    Add new view /activities/delete

--- lmkp/views/sample_values.py
+++ lmkp/views/sample_values.py
@@ -14,7 +14,7 @@ import transaction
 
 log = logging.getLogger(__name__)
 
-@view_config(route_name='sample_values', renderer='json')
+@view_config(route_name='sample_values', renderer='json', permission='administer')
 def insert_landmatrix(request):
 
     activity_protocol2 = ActivityProtocol2(Session)
@@ -27,19 +27,53 @@ def insert_landmatrix(request):
     if 'activities' not in data:
         return HTTPBadRequest(detail="Not a valid format")
 
-    for activity in data['activities']:
+    for activity in data['activities'][1:5]:
         activity_protocol2._handle_activity(request, activity)
 
     # Set the status of the newly imported activities to active
-    for activity in data['activities']:
-        if 'id' in activity:
-            identifier = activity['id']
-            db_a = Session.query(Activity).filter(Activity.activity_identifier == identifier).order_by(desc(Activity.version)).first()
+    #for activity in data['activities']:
+     #   if 'id' in activity:
+     #       identifier = activity['id']
+     ##       db_a = Session.query(Activity).filter(Activity.activity_identifier == identifier).order_by(desc(Activity.version)).first()
 
-            active_id = Session.query(Status.id).filter(Status.name == "active").first()
-            db_a.fk_status = active_id
+     #       active_id = Session.query(Status.id).filter(Status.name == "active").first()
+      #      db_a.fk_status = active_id
 
     return {'success': True}
+
+@view_config(route_name='activities_delete', renderer='lmkp:templates/sample_values.pt', permission='administer')
+def delete_activities(request):
+    
+    stack = []
+
+    all_activities = Session.query(Activity)
+    act_counter = 0
+    tag_counter = 0
+    ch_counter = 0
+    for aa in all_activities:
+        # delete tag groups
+        tag_groups = aa.tag_groups
+        tag_groups.main_tag = None
+        for tg in tag_groups:
+            tags = tg.tags
+            for t in tags:
+                tag_counter += 1
+                Session.delete(t)
+            Session.delete(tg)
+        # delete changesets
+        changesets = aa.changesets
+        for ch in changesets:
+            ch_counter += 1
+            Session.delete(ch)
+        # delete activities
+        act_counter += 1
+        Session.delete(aa)
+    if (tag_counter > 0 or act_counter > 0 or ch_counter > 0):
+        stack.append(str(tag_counter) + " a_tags deleted.")
+        stack.append(str(ch_counter) + " a_changesets deleted.")
+        stack.append(str(act_counter) + " activities deleted.")
+
+    return {'messagestack': stack}
 
 #@view_config(route_name='sample_values', renderer='lmkp:templates/sample_values.pt')
 def sample_values(request):
@@ -687,7 +721,7 @@ def delete_sample_values(request):
         stack.append(str(rev_counter) + " sh_changeset_reviews deleted.")
     #all_activities = Session.query(Activity).join(A_Changeset).filter(or_(A_Changeset.source.like('[active] Source %'), A_Changeset.source.like('[pending] Source %'), A_Changeset.source.like('[overwritten] Source %'), A_Changeset.source.like('[deleted] Source %'), A_Changeset.source.like('[overwritten] Source %'))).all()
     # Select all activities
-    all_activities = Session.query(Activity                                                                                                                                                                                                         )
+    all_activities = Session.query(Activity)
     act_counter = 0
     tag_counter = 0
     ch_counter = 0
