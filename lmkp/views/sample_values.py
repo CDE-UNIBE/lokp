458178fd5f4d71a636d43b5e84ad14044a457e58
lukas.vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2012-06-29 08:39:02 +0200

    Added more files for testing

--- lmkp/views/sample_values.py
+++ lmkp/views/sample_values.py
@@ -111,7 +111,20 @@ def test_sample_values(request):
     # Delete an Involvement
     #i3 = activity_wrapper(request, "%s/documents/temp/%s" % (rootdir, 'i3.json'), 'pending')
     # Update Stakeholder
-    s2 = stakeholder_wrapper(request, "%s/documents/temp/%s" % (rootdir, 's2.json'), 'pending')
+    #s2 = stakeholder_wrapper(request, "%s/documents/temp/%s" % (rootdir, 's2.json'), 'pending')
+    ###
+    # Create many new Stakeholders
+    #ms1 = stakeholder_wrapper(request, "%s/documents/temp/%s" % (rootdir, 'ms1.json'), 'active')
+    # Create many new Activities with links to Stakholders
+    #mai1 = activity_wrapper(request, "%s/documents/temp/%s" % (rootdir, 'mai1.json'), 'active')
+    """
+    UPDATE data.stakeholders
+    SET fk_status = 3
+    """
+    # Update Activity 1
+    #a6 = activity_wrapper(request, "%s/documents/temp/%s" % (rootdir, 'a6.json'), 'pending')
+    # Remove an Involvement
+    #s3 = stakeholder_wrapper(request, "%s/documents/temp/%s" % (rootdir, 's3.json'), 'pending')
 
     return {'success': True}
 
