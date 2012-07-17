3bc2f80e964bae9096ce8e0b7d5b36e00195aca6
lukas.vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2012-07-17 11:11:36 +0200

    Ajax request but no ActivityPanel yet

--- lmkp/static/app/view/moderator/Review.js
+++ lmkp/static/app/view/moderator/Review.js
@@ -13,7 +13,39 @@ Ext.define('Lmkp.view.moderator.Review', {
     
     items: [{
         xtype: 'panel',
+        name: 'review_initial',
         html: 'Select an item on the left.',
         border: 0
-    }]
+    }],
+
+    updateContentActivity: function(panel, data) {
+        console.log(data);
+        console.log("Question: 2 functions (Activity / Stakeholder) needed?");
+
+        // remove the initial panel
+        if (panel.down('panel[name=review_initial]')) {
+            panel.remove(panel.down('panel[name=review_initial]'));
+        }
+
+        // remove existing review panel (activity)
+        while (panel.down('panel[name=review_activity]')) {
+            panel.remove(panel.down('panel[name=review_activity]'));
+        }
+
+        for (var i in data.data) {
+            if (data.data[i].status == 'pending') {
+                var newPanel = Ext.create('Ext.panel.Panel', {
+                    name: 'review_activity',
+                    html: data.data[i].source
+                });
+                panel.add(newPanel);
+            }
+        }
+    },
+
+    updateContentStakeholder: function(panel, data) {
+        console.log(data);
+        console.log("coming soon");
+        console.log("Question: 2 functions (Activity / Stakeholder) needed?");
+    }
 });
