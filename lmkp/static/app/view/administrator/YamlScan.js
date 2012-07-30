dd490545e66cf2a0dd5c1ad4949dfd9a2874002f
Adrian Weber <adrian.weber@cde.unibe.ch>
2012-07-30 14:40:21 +0200

    Reduce nesting in YamlScan

--- lmkp/static/app/view/administrator/YamlScan.js
+++ lmkp/static/app/view/administrator/YamlScan.js
@@ -1,5 +1,5 @@
 Ext.define('Lmkp.view.administrator.YamlScan', {
-    extend: 'Ext.panel.Panel',
+    extend: 'Ext.tree.Panel',
 
     alias: ['widget.yamlscanpanel'],
     
@@ -23,114 +23,102 @@ Ext.define('Lmkp.view.administrator.YamlScan', {
         xtype: 'toolbar'
     }],
 
-    initComponent: function() {
-        // Call first the parent method
-        this.callParent(arguments);
-
-        // Then add the items to this container
-        this.add([{
-            xtype: 'treepanel',
-            store: this.getStore(),
-            rootVisible: false,
-            // TODO: autoScroll not yet working properly. Although scroll bar appears, it does so too late.
-            autoScroll: true,
-            columns: [{
-                xtype: 'treecolumn',
-                header: 'Original',
-                flex: 1,
-                dataIndex: 'value',
-                sortable: true
-            },{
-                xtype: 'templatecolumn',
-                name: 'mandatory',
-                text: 'Mandatory',
-                flex: 1,
-                sortable: true,
-                dataIndex: 'mandatory',
-                align: 'center',
-                tpl: Ext.create('Ext.XTemplate', '{[this.isMandatory(values.mandatory)]}', {
-                    isMandatory: function(m) {
-                        if (m) {
-                            return 'yes';
-                        } else {
-                            return '<i>no</i>';
-                        }
+    store: this.store,
+    rootVisible: false,
+    // TODO: autoScroll not yet working properly. Although scroll bar appears, it does so too late.
+    autoScroll: true,
+    columns: [{
+        xtype: 'treecolumn',
+        header: 'Original',
+        flex: 1,
+        dataIndex: 'value',
+        sortable: true
+    },{
+        xtype: 'templatecolumn',
+        name: 'mandatory',
+        text: 'Mandatory',
+        flex: 1,
+        sortable: true,
+        dataIndex: 'mandatory',
+        align: 'center',
+        tpl: Ext.create('Ext.XTemplate', '{[this.isMandatory(values.mandatory)]}', {
+            isMandatory: function(m) {
+                if (m) {
+                    return 'yes';
+                } else {
+                    return '<i>no</i>';
+                }
+            }
+        })
+    }, {
+        xtype: 'templatecolumn',
+        name: 'local',
+        text: 'Local YAML',
+        flex: 1,
+        sortable: true,
+        dataIndex: 'local',
+        align: 'center',
+        tpl: Ext.create('Ext.XTemplate', '{[this.isLocal(values.local)]}', {
+            isLocal: function(l) {
+                if (l) {
+                    return 'yes';
+                } else {
+                    return 'no';
+                }
+            }
+        })
+    }, {
+        xtype: 'templatecolumn',
+        text: 'In DB',
+        flex: 1,
+        sortable: true,
+        dataIndex: 'exists',
+        align: 'center',
+        tpl: Ext.create('Ext.XTemplate', '{[this.isInDB(values.exists)]}', {
+            isInDB: function(e) {
+                if (e) {
+                    return 'yes';
+                } else {
+                    return '<b>no</b>';
+                }
+            }
+        })
+    }, {
+        xtype: 'templatecolumn',
+        header: 'Translation',
+        flex: 1,
+        dataIndex: 'translation',
+        sortable: true,
+        tpl: Ext.create('Ext.XTemplate', '{[this.showTranslation(values.translation)]}', {
+            showTranslation: function(t) {
+                if (t == 1) {			// not yet translated
+                    return '-';
+                } else if (t == 0) {	// already in english
+                    return '[already translated]';
+                } else {				// show translation
+                    return t;
+                }
+            }
+        })
+    }, {
+        xtype: 'templatecolumn',
+        name: 'editColumn',
+        width: 25,
+        align: 'center',
+        tpl: Ext.create('Ext.XTemplate', '{[this.showTranslationButton(values.exists, values.translation)]}', {
+            showTranslationButton: function(e, t) {
+                if (e) {		// only show buttons when original in database
+                    if (t == 1) {			// not yet translated
+                        return '<img src="static/img/application_form_add.png" title="add translation" alt="add translation"/>';
+                    } else if (t == 0) {	// already in english
+                        return '';
+                    } else {				// show translation
+                        return '<img src="static/img/application_form_edit.png" title="edit translation" alt="edit translation"/>';
                     }
-                })
-            }, {
-                xtype: 'templatecolumn',
-                name: 'local',
-                text: 'Local YAML',
-                flex: 1,
-                sortable: true,
-                dataIndex: 'local',
-                align: 'center',
-                tpl: Ext.create('Ext.XTemplate', '{[this.isLocal(values.local)]}', {
-                    isLocal: function(l) {
-                        if (l) {
-                            return 'yes';
-                        } else {
-                            return 'no';
-                        }
-                    }
-                })
-            }, {
-                xtype: 'templatecolumn',
-                text: 'In DB',
-                flex: 1,
-                sortable: true,
-                dataIndex: 'exists',
-                align: 'center',
-                tpl: Ext.create('Ext.XTemplate', '{[this.isInDB(values.exists)]}', {
-                    isInDB: function(e) {
-                        if (e) {
-                            return 'yes';
-                        } else {
-                            return '<b>no</b>';
-                        }
-                    }
-                })
-            }, {
-                xtype: 'templatecolumn',
-                header: 'Translation',
-                flex: 1,
-                dataIndex: 'translation',
-                sortable: true,
-                tpl: Ext.create('Ext.XTemplate', '{[this.showTranslation(values.translation)]}', {
-                    showTranslation: function(t) {
-                        if (t == 1) {			// not yet translated
-                            return '-';
-                        } else if (t == 0) {	// already in english
-                            return '[already translated]';
-                        } else {				// show translation
-                            return t;
-                        }
-                    }
-                })
-            }, {
-                xtype: 'templatecolumn',
-                name: 'editColumn',
-                width: 25,
-                align: 'center',
-                tpl: Ext.create('Ext.XTemplate', '{[this.showTranslationButton(values.exists, values.translation)]}', {
-                    showTranslationButton: function(e, t) {
-                        if (e) {		// only show buttons when original in database
-                            if (t == 1) {			// not yet translated
-                                return '<img src="static/img/application_form_add.png" title="add translation" alt="add translation"/>';
-                            } else if (t == 0) {	// already in english
-                                return '';
-                            } else {				// show translation
-                                return '<img src="static/img/application_form_edit.png" title="edit translation" alt="edit translation"/>';
-                            }
-                        } else {
-                            return '';
-                        }
-                    }
-                })
-            }]
-        }]
-        );
-
-        
-    }
+                } else {
+                    return '';
+                }
+            }
+        })
+    }]
 })
