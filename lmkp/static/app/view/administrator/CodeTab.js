2d50fd3aa126eee594b6721edbfd0c09c1c310a2
lukas.vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2012-07-30 15:26:17 +0200

    Codes can be inserted by administrators

---
+++ lmkp/static/app/view/administrator/CodeTab.js
@@ -0,0 +1,133 @@
+Ext.define('Lmkp.view.administrator.CodeTab', {
+    extend: 'Ext.panel.Panel',
+    alias: ['widget.lo_administratorcodetab'],
+
+    layout: 'border',
+    defaults: {
+        bodyPadding: 5
+    },
+
+    items: [
+        {
+            xtype: 'form',
+            url: 'codes/add',
+            region: 'north',
+            layout: 'column',
+            items: [
+                {
+                    xtype: 'fieldset',
+                    title: 'Input',
+                    columnWidth: 1/2,
+                    margin: '0 5 0 0',
+                    defaults: {
+                        bodyPadding: 5,
+                        layout: 'anchor'
+                    },
+                    items: [
+                        {
+                            xtype: 'panel',
+                            layout: 'hbox',
+                            border: 0,
+                            anchor: '100%',
+                            items: [
+                                {
+                                    xtype: 'combobox',
+                                    fieldLabel: 'File',
+                                    itemId: 'code_file_combobox',
+                                    displayField: 'filename',
+                                    valueField: 'filename',
+                                    name: 'filename',
+                                    queryMode: 'local',
+                                    flex: 1
+                                }, {
+                                    xtype: 'button',
+                                    itemId: 'code_file_scan',
+                                    text: 'scan',
+                                    flex: 0
+                                }
+                            ]
+                        }, {
+                            xtype: 'component',
+                            html: 'or',
+                            margin: '0 0 5 0'
+                        }, {
+                            xtype: 'textarea',
+                            fieldLabel: 'Text',
+                            name: 'code_text',
+                            emptyText: '[code] [delimiter] [value] (Not yet supported)',
+                            anchor: '100%'
+                        }
+                    ]
+                }, {
+                    xtype: 'panel',
+                    border: 0,
+                    columnWidth: 1/2,
+                    items: [
+                        {
+                            xtype: 'fieldset',
+                            title: 'Settings',
+                            defaults: {
+                                layout: 'anchor'
+                            },
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    fieldLabel: 'Description',
+                                    itemId: 'description',
+                                    anchor: '100%'
+                                }, {
+                                    xtype: 'textfield',
+                                    fieldLabel: 'Delimiter',
+                                    itemId: 'delimiter',
+                                    name: 'delimiter',
+                                    anchor: '100%',
+                                    allowBlank: false
+                                }, {
+                                    xtype: 'combobox',
+                                    fieldLabel: 'Value type',
+                                    store: ['activity', 'stakeholder'],
+                                    itemId: 'value_type',
+                                    name: 'item_type',
+                                    anchor: '100%',
+                                    allowBlank: false
+                                }
+                            ]
+                        }, {
+                            xtype: 'panel',
+                            border: 0,
+                            layout: 'hbox',
+                            items: [
+                                {
+                                    xtype: 'component',
+                                    flex: 1
+                                }, {
+                                    xtype: 'button',
+                                    formBind: true,
+                                    disabled: true,
+                                    text: 'Go',
+                                    flex: 0,
+                                    itemId: 'code_submit_button'
+                                }
+                            ]
+
+                        }
+                    ]
+                }
+            ]
+        }, {
+            xtype: 'textarea',
+            itemId: 'feedback_textarea',
+            region: 'center',
+            readOnly: true,
+            margin: 5
+        }
+    ],
+    bbar: [{
+        xtype: 'panel',
+        itemId: 'code_statusbar',
+        html: 'Status: Ready',
+        frame: false,
+        border: false,
+        bodyStyle: 'background:transparent;'
+    }]
+});
