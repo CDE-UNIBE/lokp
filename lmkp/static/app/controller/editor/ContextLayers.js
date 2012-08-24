d1ebfe9d702eaddba73db0ac29ec7e04d4a78fe8
Adrian Weber <adrian.weber@cde.unibe.ch>
2012-08-24 11:39:41 +0200

    Add button to menu entry to show legend ticket:105

--- lmkp/static/app/controller/editor/ContextLayers.js
+++ lmkp/static/app/controller/editor/ContextLayers.js
@@ -7,14 +7,42 @@ Ext.define('Lmkp.controller.editor.ContextLayers', {
 
     init: function() {
         this.control({
-            'lo_contextlayers lo_layercheckitem':{
-                checkchange: this.onMenuCheckChange
+            'lo_contextlayers lo_layercheckitem': {
+                checkchange: this.onMenuCheckChange,
+                showlegend: this.onMenuShowLegend
             }
         });
     },
 
     onMenuCheckChange: function(item, checked, eOpts){
         item.layer.setVisibility(checked);
+    },
+
+    /**
+     * Opens a new window with the legend graphic returned by the WMS service.
+     */
+    onMenuShowLegend: function(item, layer){
+
+        var width = 100;
+
+        // Construct the WMS GetLegendGraphic URL
+        var imgsrc = layer.url
+        + '?service=WMS&version=1.1.0&request=GetLegendGraphic&FORMAT=image/png&width='
+        + width
+        + '&layer='
+        + layer.params.LAYERS
+        + '&style='
+        + layer.params.STYLES
+        Ext.create('Ext.window.Window', {
+            title: layer.name,
+            layout: 'fit',
+            hideMode: 'display',
+            items: {
+                html: '<img src="' + imgsrc + '">',
+                padding: 5,
+                xtype: 'container'
+            }
+        }).show();
     }
 
 });
