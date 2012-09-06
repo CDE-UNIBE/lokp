90773033cde0eeeb17f3444730d7afe53c1faef9
lukas.vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2012-09-06 08:50:27 +0200

    Revamp of form to add new Stakeholders

--- lmkp/static/app/controller/stakeholders/NewStakeholder.js
+++ lmkp/static/app/controller/stakeholders/NewStakeholder.js
@@ -5,13 +5,34 @@ Ext.define('Lmkp.controller.stakeholders.NewStakeholder', {
     'stakeholders.NewStakeholder'
     ],
 
+    refs: [
+        {
+            ref: 'newStakeholderForm',
+            selector: 'lo_newstakeholderpanel form[itemId=newStakeholderForm]'
+        }
+    ],
+
     init: function(){
+        // Use some functionalities already defined in
+        // controller.activities.NewActivity
+        var newActivityController = this.getController('activities.NewActivity');
         this.control({
             'lo_newstakeholderpanel button[itemId="cancelButton"]': {
                 click: this.onCancelButtonClick
             },
             'lo_newstakeholderpanel button[itemId="submitButton"]': {
                 click: this.onSubmitButtonClick
+            },
+            'lo_newstakeholderpanel button[name=addAdditionalTagButton]': {
+                click: newActivityController.onAddAdditionalTagButtonClick
+            },
+            'lo_newstakeholderpanel button[itemId=addAdditionalTaggroupButton]': {
+                click: this.onAddAdditionalTaggroupButtonClick
+            },
+            // Intercept normal functionality of button (defined in
+            // Lmkp.view.activities.NewTaggroupPanel)
+            'lo_newstakeholderpanel button[name=deleteTag]': {
+                click: newActivityController.onDeleteTagButtonClick
             }
         });
     },
@@ -22,102 +43,116 @@ Ext.define('Lmkp.controller.stakeholders.NewStakeholder', {
 
     onSubmitButtonClick: function(button, event, eOpts){
         var me = this;
-        var form = button.up('form');
-        var p = button.up('lo_newstakeholderpanel');
-        if (form.getForm().isValid()) {
+        var formpanel = this.getNewStakeholderForm();
+        var taggroups = [];
 
-            // The form cannot be submitted 'normally' because ActivityProtocol expects a JSON object.
-            // As a solution, the form values are used to create a JSON object which is sent using an
-            // AJAX request.
-            // http://www.sencha.com/forum/showthread.php?132082-jsonData-in-submit-action-of-form
-
-            // collect values and fill them into TagGroups
-            // TODO: it seems that the main tag (first added to dict) always remains in first position, but
-            // maybe this should be ensured in a better way ...
-            var taggroups = [];
-
-            for (var i in form.getValues()) {
-                var tags = [];
-                var main_tag = {};
-                // first, look only at mandatory fields (no '__val' or '__attr' in name)
-                if (i.indexOf("__attr") == -1 && i.indexOf("__val") == -1) {
-                    var tag = {};
-                    tag['key'] = i;
-                    tag['value'] = form.getValues()[i];
-                    tag['op'] = 'add';
-                    tags.push(tag);
-                    // also add to main_tag
-                    main_tag['key'] = i;
-                    main_tag['value'] = form.getValues()[i];
-
-                    // look if further attributes to this field were entered
-                    var attrs = Ext.ComponentQuery.query('[name=' + i + '__attr]');
-                    var vals = Ext.ComponentQuery.query('[name=' + i + '__val]');
-                    if (attrs.length > 0 && vals.length > 0 && attrs.length == vals.length) {
-                        for (var j=0; j<attrs.length; j++) {
-                            var tag = {};
-                            tag['key'] = attrs[j].getValue();
-                            tag['value'] = vals[j].getValue();
-                            tag['op'] = 'add';
-                            tags.push(tag);
-                        }
-                    }
-                }
-                if (tags.length > 0) {
-                    taggroups.push({
-                        'tags': tags,
-                        'main_tag': main_tag
+        // Loop through each form panel
+        var forms = formpanel.query('form[name=taggroupfieldset]');
+        for (var i in forms) {
+            var tags = [];
+            var main_tag = new Object();
+            // Within a taggroup, loop through each tag
+            var tgpanels = forms[i].query('lo_newtaggrouppanel');
+            for (var j in tgpanels) {
+                var c = tgpanels[j];
+                // Only add Tags where Value or Key are not empty
+                if (c.getKeyValue() != null && c.getValueValue() != null) {
+                    tags.push({
+                        'key': c.getKeyValue(),
+                        'value': c.getValueValue(),
+                        'op': 'add'
                     });
+                    if (c.isMainTag()) {
+                        main_tag.key = c.getKeyValue();
+                        main_tag.value = c.getValueValue();
+                    }
                 }
             }
-            var diffObject = {
-                'stakeholders': [{
-                    'taggroups': taggroups
-                }]
-            };
+            if (tags.length > 0) {
+                taggroups.push({
+                    'tags': tags,
+                    'main_tag': main_tag
+                });
+            }
+        }
 
-            // send JSON through AJAX request
-            Ext.Ajax.request({
-                url: '/stakeholders',
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8'
-                },
-                jsonData: diffObject,
-                callback: function(options, success, response) {
-                    if(success) {
-                        Ext.Msg.alert('Success', 'The stakeholder was successfully created. It will be reviewed shortly.');
+        // Put together the diff object.
+        var diffObject = {
+            'stakeholders': [{
+                'taggroups': taggroups
+            }]
+        };
 
-                        // Put newly created Stakeholder into a store.
-                        var store = Ext.create('Ext.data.Store', {
-                            autoLoad: true,
-                            model: 'Lmkp.model.Stakeholder',
-                            data : Ext.decode(response.responseText),
-                            proxy: {
-                                type: 'memory',
-                                reader: {
-                                    root: 'data',
-                                    type: 'json',
-                                    totalProperty: 'total'
-                                }
+        // send the diff JSON through AJAX request
+        Ext.Ajax.request({
+            url: '/stakeholders',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            jsonData: diffObject,
+            callback: function(options, success, response) {
+                if (success) {
+                    Ext.Msg.alert('Success', 'The stakeholder was successfully created. It will be reviewed shortly.');
+
+                    // Put newly created Stakeholder into a store.
+                    var store = Ext.create('Ext.data.Store', {
+                        autoLoad: true,
+                        model: 'Lmkp.model.Stakeholder',
+                        data : Ext.decode(response.responseText),
+                        proxy: {
+                            type: 'memory',
+                            reader: {
+                                root: 'data',
+                                type: 'json',
+                                totalProperty: 'total'
                             }
-                        });
+                        }
+                    });
 
-                        // Add newly created Stakeholder to fieldset in other
-                        // window
-                        var newActivityController =
-                            me.getController('activities.NewActivity');
-                        newActivityController._onNewStakeholderCreated(
-                            store.getAt(0)
-                        );
+                    // Add newly created Stakeholder to fieldset in other
+                    // window
+                    var newActivityController =
+                        me.getController('activities.NewActivity');
+                    newActivityController._onNewStakeholderCreated(
+                        store.getAt(0)
+                    );
 
-                    } else {
-                        Ext.Msg.alert('Failure', 'The stakeholder could not be created.');
-                        this.setAddedStakeholder(null);
-                    }
-                    this.up('window').close();
-                },
-                scope: p
+                    // Close form window
+                    formpanel.up('window').close();
+                    
+                } else {
+                    Ext.Msg.alert('Failure', 'The stakeholder could not be created.');
+                }
+            },
+            scope: this
+        });
+    },
+
+    /**
+     * Adds an additional TagGroup panel to the form.
+     */
+    onAddAdditionalTaggroupButtonClick: function(button) {
+        var form = button.up('form');
+        var panel = form.up('panel');
+
+        if (form && panel) {
+            // create the stores
+            var mainStore = Ext.create('Lmkp.store.StakeholderConfig');
+            mainStore.load(function() {
+                // Create and load a second store with all keys
+                var completeStore = Ext.create('Lmkp.store.StakeholderConfig');
+                completeStore.load(function() {
+                    // When loaded, show panel
+                    var fieldset = panel._getFieldset();
+                    fieldset.add(panel._getSingleFormItem(
+                        mainStore,
+                        completeStore,
+                        null
+                    ));
+                    // Insert it always at the bottom.
+                    form.insert(form.items.length, fieldset);
+                });
             });
         }
     }
