dcaffa8817149bc9d82c117afc7744813d468599
Adrian Weber <adrian.weber@cde.unibe.ch>
2012-10-18 16:21:28 +0200

    Filter on store needs to be cleared, should fix ticket:137

--- lmkp/static/app/view/public/FilterActivityWindow.js
+++ lmkp/static/app/view/public/FilterActivityWindow.js
@@ -12,7 +12,7 @@ Ext.define('Lmkp.view.public.FilterActivityWindow', {
     modal: true,
 
     title: Lmkp.ts.msg('activities_filter-title'),
-    width: 550,
+    width: 570,
 
     items: [
         {
