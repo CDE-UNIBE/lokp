5a51662bf8fdd37cc488f8d3192a80b6de8e5a8c
lukas.vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2013-02-05 07:54:52 +0100

    Removed functionality of user changesets to prevent error message

--- lmkp/views/changeset_protocol.py
+++ lmkp/views/changeset_protocol.py
@@ -16,27 +16,32 @@ class ChangesetProtocol(object):
         
         """
 
-        # Join the activity changesets with the activities and the users
-        a_changesetsJoins = self.Session.query(A_Changeset.source, Activity.activity_identifier.label("activity"), User.username, Status.name).join(A_Changeset.user).join(A_Changeset.activity).join(Status)
+        #TODO: Revamp this!
+        # 2013/02/05: Commented everything to prevent error message.
 
-        # filters first
-        a_changesetsJoins = a_changesetsJoins.filter(self._filter_user(request)).filter(self._filter_status(request))
+#        # Join the activity changesets with the activities and the users
+#        a_changesetsJoins = self.Session.query(A_Changeset.source, Activity.activity_identifier.label("activity"), User.username, Status.name).join(A_Changeset.user).join(A_Changeset.activity).join(Status)
+#
+#        # filters first
+#        a_changesetsJoins = a_changesetsJoins.filter(self._filter_user(request)).filter(self._filter_status(request))
+#
+#        # Create a list of activity changesets
+#        activities = []
+#        for i in a_changesetsJoins.limit(self._limit_changesets(request)).offset(self._offset_changesets(request)):
+#            activities.append({'source': i[0], 'activity': str(i[1]), 'user': i[2], 'status': i[3]})
+#        #activities.append({'total': a_changesetsJoins.count()})
+#
+#        # Join the stakeholder changesets with the stakeholders and the users
+#        sh_changesetsJoins = self.Session.query(SH_Changeset.source, Stakeholder.stakeholder_identifier.label("stakeholder"), User.username, Status.name).join(SH_Changeset.user).join(SH_Changeset.stakeholder).join(Status)
+#
+#        # Create a list of stakeholder changesets
+#        stakeholders = []
+#        for i in sh_changesetsJoins.filter(self._filter_user(request)).filter(self._filter_status(request)).limit(self._limit_changesets(request)):
+#            stakeholders.append({'source': i[0], 'stakeholder': str(i[1]), 'user': i[2], 'status': i[3]})
+#
+#        return {'activities': activities, 'activities_count': a_changesetsJoins.count(), 'stakeholders': stakeholders}
 
-        # Create a list of activity changesets
-        activities = []
-        for i in a_changesetsJoins.limit(self._limit_changesets(request)).offset(self._offset_changesets(request)):
-            activities.append({'source': i[0], 'activity': str(i[1]), 'user': i[2], 'status': i[3]})
-        #activities.append({'total': a_changesetsJoins.count()})
-
-        # Join the stakeholder changesets with the stakeholders and the users
-        sh_changesetsJoins = self.Session.query(SH_Changeset.source, Stakeholder.stakeholder_identifier.label("stakeholder"), User.username, Status.name).join(SH_Changeset.user).join(SH_Changeset.stakeholder).join(Status)
-
-        # Create a list of stakeholder changesets
-        stakeholders = []
-        for i in sh_changesetsJoins.filter(self._filter_user(request)).filter(self._filter_status(request)).limit(self._limit_changesets(request)):
-            stakeholders.append({'source': i[0], 'stakeholder': str(i[1]), 'user': i[2], 'status': i[3]})
-
-        return {'activities': activities, 'activities_count': a_changesetsJoins.count(), 'stakeholders': stakeholders}
+        return {}
 
     def _filter_user(self, request):
         """
