39276e77cd67e4320106f76f0c55f7556bf7fca9
lukas.vonlanthen <lukas.vonlanthen@cde.unibe.ch>
2013-02-12 17:40:36 +0100

    Version 0.3

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requires = [
     ]
 
 setup(name='LMKP',
-      version='0.2',
+      version='0.3',
       description='The Land Matrix Knowledge Platform',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
