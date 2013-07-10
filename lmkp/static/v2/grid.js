ff98543561ef7e17bdea9906304e874b69c3da70
lvonlanthen <lukas.vonlanthen@cde.unibe.ch>
2013-07-10 13:36:42 +0200

    Temporarily switching on link to involvements in grid for screengrab

--- lmkp/static/v2/grid.js
+++ lmkp/static/v2/grid.js
@@ -3,21 +3,21 @@
  * Shows a popup on the same line as the selected row.
  */
 $('#activitygrid tbody>tr').click(function() {
-//    $(this).parent().find('tr').removeClass('active-row');
-//    $(this).addClass('active-row');
-//
-//    var identifier = $(this).find('td.identifier').html();
-//    if (!identifier) return;
-//
-//    if ($('div.show-investors.visible-phone').is(':visible')) {
-//        // Switch directly
-//        // TODO
-//        console.log("switch");
-//    } else {
-//        var popup = $('.show-investors-wrapper');
-//        var position = $(this).position().top - $('#activitygrid').position().top - 5;
-//        popup.removeClass('hidden').css({'margin-top': position});
-//
-//        var l = popup.find('a').attr('href', '/stakeholders/grid');
-//    }
+    $(this).parent().find('tr').removeClass('active-row');
+    $(this).addClass('active-row');
+
+    var identifier = $(this).find('td.identifier').html();
+    if (!identifier) return;
+
+    if ($('div.show-investors.visible-phone').is(':visible')) {
+        // Switch directly
+        // TODO
+        console.log("switch");
+    } else {
+        var popup = $('.show-investors-wrapper');
+        var position = $(this).position().top - $('#activitygrid').position().top - 5;
+        popup.removeClass('hidden').css({'margin-top': position});
+
+        var l = popup.find('a').attr('href', '/stakeholders/grid');
+    }
 });
