8057368b14355c78bda8dffc9880c5655d7df824
lvonlanthen <lukas.vonlanthen@cde.unibe.ch>
2014-11-12 15:41:48 +0100

    Release v0.9.8b

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ requires = [
 ]
 
 setup(name='LMKP',
-      version='0.9.8a',
+      version='0.9.8b',
       description='The Land Observatory',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
