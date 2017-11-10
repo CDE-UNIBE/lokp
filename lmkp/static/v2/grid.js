f483fed5218a9b670934a85110e5c16fc6a79668
Lukas Vonlanthen <lukas.vonlanthen@gmail.com>
2017-11-10 13:50:17 +0100

    Fix link to stakeholders by activities and vice versa

--- lmkp/static/v2/grid.js
+++ lmkp/static/v2/grid.js
@@ -4,34 +4,38 @@
  * other side of the involvements.
  * On small screens, the popup appears on top of the selected row.
  */
-$('#itemgrid tbody>tr').tooltip({
-  html: true,
-  placement: function(tip, element) {
-    var padding_right = $('.item-grid-wrapper').css('padding-right')
-      .replace("px", "");
-    if (padding_right > 20) {
-      return 'right';
-    }
-    return "top";
-  },
-  title: '<a href="#">' + link_involvement_text + '</a>',
-  trigger: 'manual'
-});
-$('#itemgrid tbody>tr').on('click', function() {
-  var identifier = $(this).find('td.identifier').html();
-  if (!identifier) return;
-  var itemType = $(this).find('td.itemType').html();
-  if (!itemType) return;
-  var other = (itemType == 'stakeholders') ? 'activities' : 'stakeholders';
-  var by = (itemType == 'stakeholders') ? 'bystakeholders' : 'byactivities';
-  var url = '/' + [other, by, 'html', identifier].join('/');
-  window.location.href = url;
+// $('#itemgrid tbody>tr').tooltip({
+//   html: true,
+//   placement: function(tip, element) {
+//     var padding_right = $('.item-grid-wrapper').css('padding-right')
+//       .replace("px", "");
+//     if (padding_right > 20) {
+//       return 'right';
+//     }
+//     return "top";
+//   },
+//   title: '<a href="#">' + link_involvement_text + '</a>',
+//   trigger: 'manual'
+// });
+// $('#itemgrid tbody>tr').on('click', function() {
+//   var identifier = $(this).find('td.identifier').html();
+//   if (!identifier) return;
+//   var itemType = $(this).find('td.itemType').html();
+//   if (!itemType) return;
+//   var other = (itemType == 'stakeholders') ? 'activities' : 'stakeholders';
+//   var by = (itemType == 'stakeholders') ? 'bystakeholders' : 'byactivities';
+//   var url = '/' + [other, by, 'html', identifier].join('/');
+//   window.location.href = url;
+//
+//   /*$('#itemgrid tbody>tr').not(this).tooltip('hide');
+//   $(this).tooltip('toggle');
+//   $('.tooltip a').attr('href', url);*/
+// });
 
-  /*$('#itemgrid tbody>tr').not(this).tooltip('hide');
-  $(this).tooltip('toggle');
-  $('.tooltip a').attr('href', url);*/
-});
+// $(window).resize(function() {
+//   $('#itemgrid tbody>tr').tooltip('hide');
+// });
 
-$(window).resize(function() {
-  $('#itemgrid tbody>tr').tooltip('hide');
+$('.js-list-row-link').click(function() {
+    window.location = $(this).data("href");
 });
